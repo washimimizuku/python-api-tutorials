ec61bc222c6c0c907c97b4ab9b74c204857d17a5
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-09 15:36:17 +0100

    Add needed parameter

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ async def read_user(user_id: str):
 
 
 @app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
+async def read_user_item(user_id: int, item_id: str, needy: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id, "needy": needy}
     if q:
         item.update({"q": q})
     if not short:

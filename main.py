23999a38f64b78f0a9c7ae3707f6f36fadfd9e6f
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-09 15:58:54 +0100

    Order endpoints

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 app = FastAPI()
@@ -60,6 +60,22 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
     return item
 
 
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
@@ -80,20 +96,4 @@ async def read_user_item(user_id: int, item_id: str, needy: str, q: Optional[str
             {"description": "This is an amazing item that has a long description"}
         )
     return item
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
 

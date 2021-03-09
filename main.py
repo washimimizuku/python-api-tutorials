6c04f4239e0d925f39f13a86e5a259212b25acce
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-09 15:22:26 +0100

    Add list item endpoint

--- main.py
+++ main.py
@@ -8,21 +8,28 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
+
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -34,6 +41,12 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
+
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/items/")
+async def list_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]

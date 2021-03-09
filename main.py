0b1cdc8f59811e6387944b8ad64287aaa655e94b
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-09 15:33:50 +0100

    Add multiple path and query parameters example

--- main.py
+++ main.py
@@ -44,6 +44,18 @@ async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:

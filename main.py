4f9e5891fe259487a27cd370fb5dd6fa252d0c05
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-09 15:55:44 +0100

    Add update item endpoint

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ async def create_item(item: Item):
     return item_dict
 
 
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
     item = {"item_id": item_id}

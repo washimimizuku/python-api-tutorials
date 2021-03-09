e18286c7533b3fced69b28b1927b76b4932167f8
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-09 15:29:06 +0100

    Modify read item endpoint to support query parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from typing import Optional
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -16,9 +17,21 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/")
+async def list_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get("/users/me")
@@ -46,7 +59,3 @@ async def get_model(model_name: ModelName):
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
-
-@app.get("/items/")
-async def list_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]

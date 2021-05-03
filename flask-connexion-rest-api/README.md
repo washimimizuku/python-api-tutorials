84c931b1e3465497b5cc94265e58c4079eb11b8c
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-04 00:27:17 +0200

    Add frontend client

--- flask-connexion-rest-api/README.md
+++ flask-connexion-rest-api/README.md
@@ -8,4 +8,5 @@ A tutorial to learn and experiment with Flask: https://realpython.com/flask-conn
 $ python server.py 
 ```
 
-Go to http://127.0.0.1:5000/
+Go to: http://127.0.0.1:5000/
+Swagger docs: http://localhost:5000/api/ui/
--- flask-connexion-rest-api/server.py
+++ flask-connexion-rest-api/server.py
@@ -1,24 +1,29 @@
+"""
+Main module of the server file
+"""
+
+# 3rd party modules
 from flask import render_template
 import connexion
 
+
 # Create the application instance
-app = connexion.App(__name__, specification_dir='./')
+app = connexion.App(__name__, specification_dir="./")
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+app.add_api("swagger.yml")
 
 
-# Create a URL route in our application for "/"
-@app.route('/')
+# create a URL route in our application for "/"
+@app.route("/")
 def home():
     """
     This function just responds to the browser URL
     localhost:5000/
-    :return:        the rendered template 'home.html.jinja'
+    :return:        the rendered template "home.html.jinja"
     """
-    return render_template('home.html.jinja')
+    return render_template("home.html.jinja")
 
 
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)

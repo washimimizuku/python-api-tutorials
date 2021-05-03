72d550c594fcdc39ee5af9378bfc71100e6dfc23
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-04 00:16:41 +0200

    Add get people endpoint

--- flask-connexion-rest-api/server.py
+++ flask-connexion-rest-api/server.py
@@ -1,25 +1,24 @@
-from flask import (
-    Flask,
-    render_template
-)
+from flask import render_template
+import connexion
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
 
 # Create a URL route in our application for "/"
-
-
 @app.route('/')
 def home():
     """
     This function just responds to the browser URL
     localhost:5000/
-
-    :return:        the rendered template 'home.html'
+    :return:        the rendered template 'home.html.jinja'
     """
     return render_template('home.html.jinja')
 
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
